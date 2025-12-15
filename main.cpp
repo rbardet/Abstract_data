45efed07f0fd2ff5cc363ebfd40debb7b84836f2
rbardet <robin.bardet.tomczak@gmail.com>
2025-12-15 19:21:23 +0100

    Feat: add some test

--- main.cpp
+++ main.cpp
@@ -1,11 +1,32 @@
 #include <iostream>
 #include "vector.hpp"
-
 #include <vector>
 
-int main(void) {
-	ft::vector<int> test(10, 4);
-	
-	std::cout << "Hello, World!" << std::endl;
-	return (0);
+int main() {
+	ft::vector<int> my_vector(5, 4);
+	std::vector<int> std_vector(5, 4);
+
+	std::cout << "=== Test des valeurs dans les vecteurs ===\n";
+	for (size_t i = 0; i < my_vector.size(); ++i) {
+		std::cout << "Index " << i << " -> ft::vector: " << my_vector[i]
+				  << " | std::vector: " << std_vector[i] << std::endl;
+	}
+
+	std::cout << "\n=== Test de la capacity() ===\n";
+	std::cout << "ft::vector capacity(): " << my_vector.capacity() << std::endl;
+	std::cout << "std::vector capacity(): " << std_vector.capacity() << std::endl;
+
+	std::cout << "\n=== Test de empty() ===\n";
+	std::cout << "ft::vector empty(): " << my_vector.empty() << std::endl;
+	std::cout << "std::vector empty(): " << std_vector.empty() << std::endl;
+
+	std::cout << "\n=== Test de front() ===\n";
+	std::cout << "ft::vector front(): " << my_vector.front() << std::endl;
+	std::cout << "std::vector front(): " << std_vector.front() << std::endl;
+
+	std::cout << "\n=== Test de back() ===\n";
+	std::cout << "ft::vector back(): " << my_vector.back() << std::endl;
+	std::cout << "std::vector back(): " << std_vector.back() << std::endl;
+
+	return 0;
 }
