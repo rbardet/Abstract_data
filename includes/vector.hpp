e3fd52489f032c933e782809c80143502149877c
rbardet <robinbt2004@gmail.com>
2025-12-15 17:19:13 +0000

    Fix: compilation error

--- includes/vector.hpp
+++ includes/vector.hpp
@@ -2,21 +2,17 @@
 #define VECTOR_HPP
 
 #include <cstddef>
+#include <stdexcept>
 
 // i refer to cpp.com
 // https://cplusplus.com/reference/vector/vector/
 
 namespace ft {
-	template< typename T, typename alloc = std::allocator<T> >
+	template< typename T, typename _alloc = std::allocator<T> >
 	class vector {
-		protected:
-			pointer _M_start;
-			pointer _M_finish;
-			pointer _M_end_of_storage;
-			allocator_type _Alloc;
 		public:
 			typedef T value_type;
-			typedef alloc allocator_type;
+			typedef _alloc allocator_type;
 			typedef T& reference;
 			typedef const T& const_reference;
 			typedef T* pointer;
@@ -27,18 +23,21 @@ namespace ft {
 		explicit vector(
 			const allocator_type &alloc = allocator_type()
 		) :
-		_M_start(NULL), _M_finish(NULL), _M_end_of_storage(NULL) {};
+		_M_start(NULL), _M_finish(NULL), _M_end_of_storage(NULL), _Alloc(alloc) {}
 
 		explicit vector(
 			size_type n,
 			const value_type &val = value_type(),
 			const allocator_type &alloc = allocator_type()
 		) :
-		_M_start(), _M_finish(_M_start + n), _M_end_of_storage(_M_finish)
-		{	
-			this->_M_start = _Alloc.allocate(n);
+		_Alloc(alloc)
+		{
 			if (n > 0) {
-				for(int i=0; i < n; i++) {
+				this->_M_start = _Alloc.allocate(n);
+				this->_M_finish = this->_M_start + n;
+				this->_M_end_of_storage = this->_M_finish;
+
+				for(size_type i=0; i < n; i++) {
 					this->_M_start[i] = val;
 				}
 			}
@@ -50,8 +49,8 @@ namespace ft {
 			}
 			return *this;
 		}
+
 		~vector() {
-			this->_Alloc.destroy
 			this->_Alloc.deallocate(this->_M_start, this->size());
 		}
 
@@ -76,22 +75,27 @@ namespace ft {
 		}
 
 		reference at(size_type n) {
-			if (n >= this.size()) {
-				throw std::runtime("need to swap to a custom exception");
+			if (n >= this->size()) {
+				throw std::runtime_error("need to swap to a custom exception");
 			}
 
 			return this->_M_start[n];
 		}
 		
 		const_reference at(size_type n) const {
-			if (n >= this.size()) {
-				throw std::runtime("need to swap to a custom exception");
+			if (n >= this->size()) {
+				throw std::runtime_error("need to swap to a custom exception");
 			}
 
 			return this->_M_start[n];
 		}
 
+		protected:
+			pointer _M_start;
+			pointer _M_finish;
+			pointer _M_end_of_storage;
+			allocator_type _Alloc;
 	};
 }
 
-#_M_finishif
+#endif
--- main.cpp
+++ main.cpp
@@ -4,7 +4,8 @@
 #include <vector>
 
 int main(void) {
-	ft::vector<int> test;
+	ft::vector<int> test(10, 4);
+	
 	std::cout << "Hello, World!" << std::endl;
 	return (0);
 }
