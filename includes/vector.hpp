aae92741db14c504a270776260581cd2bbe8757a
rbardet <robin.bardet.tomczak@gmail.com>
2025-12-15 17:45:27 +0100

    save work

--- includes/vector.hpp
+++ includes/vector.hpp
@@ -7,50 +7,91 @@
 // https://cplusplus.com/reference/vector/vector/
 
 namespace ft {
-	template<typename T>
+	template< typename T, typename alloc = std::allocator<T> >
 	class vector {
-		private:
-			T *start;
-			T *end;
-			T *storage_end;
+		protected:
+			pointer _M_start;
+			pointer _M_finish;
+			pointer _M_end_of_storage;
+			allocator_type _Alloc;
 		public:
-		typedef T value_type;
-		typedef &T reference;
-		typedef const &T const_reference;
-		typedef size_t size_type;
+			typedef T value_type;
+			typedef alloc allocator_type;
+			typedef T& reference;
+			typedef const T& const_reference;
+			typedef T* pointer;
+			typedef const T* const_pointer;
+			typedef size_t size_type;
 
+		//constructor
+		explicit vector(
+			const allocator_type &alloc = allocator_type()
+		) :
+		_M_start(NULL), _M_finish(NULL), _M_end_of_storage(NULL) {};
 
-			vector() {};
-
-			vector& operator=(const vector &src) {
-				if (*this != &src) {
-
+		explicit vector(
+			size_type n,
+			const value_type &val = value_type(),
+			const allocator_type &alloc = allocator_type()
+		) :
+		_M_start(), _M_finish(_M_start + n), _M_end_of_storage(_M_finish)
+		{	
+			this->_M_start = _Alloc.allocate(n);
+			if (n > 0) {
+				for(int i=0; i < n; i++) {
+					this->_M_start[i] = val;
 				}
-				return *this;
 			}
-			~vector() {}
+		}
 
-			//Capacity
-			size_type size() const {
-				return this.end - this.start;
+		vector& operator=(const vector &x) {
+			if (*this != &x) {
+
+			}
+			return *this;
+		}
+		~vector() {
+			this->_Alloc.destroy
+			this->_Alloc.deallocate(this->_M_start, this->size());
+		}
+
+		//Capacity
+
+		size_type size() const {
+			return this->_M_finish - this->_M_start;
+		}
+
+		size_type max_size() const {
+			return this->_M_end_of_storage - this->_M_start;
+		}
+
+		//element access
+
+		reference operator[](size_type n) {
+			return this->_M_start[n];
+		}
+		
+		const_reference operator[](size_type n) const {
+			return this->_M_start[n];
+		}
+
+		reference at(size_type n) {
+			if (n >= this.size()) {
+				throw std::runtime("need to swap to a custom exception");
 			}
 
-			size_type max_size() const {
-				return this.storage_end - this.start;
+			return this->_M_start[n];
+		}
+		
+		const_reference at(size_type n) const {
+			if (n >= this.size()) {
+				throw std::runtime("need to swap to a custom exception");
 			}
 
-
-
-			//element access
-			reference operator[] (size_type n) {
-				return this.start[n];
-			}
-			
-			const_reference operator[] (size_type n) const {
-				return this.start[n];
-			}
+			return this->_M_start[n];
+		}
 
 	};
 }
 
-#endif
+#_M_finishif
